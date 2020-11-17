ac5f1f802ed2464c262fc64451b89ac01c26ae42
Insaram Alam Siddique <alaminsaram92@gmail.com>
2020-11-17 18:30:55 +0500

    Finalized Basic Operations, Designing Left

--- moneyexchange.js
+++ moneyexchange.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded',function(){
-    fetch('https://api.exchangeratesapi.io/latest?base=USD')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const rates = data.rates.EUR
-        document.querySelector('body').innerHTML = `1 USD equals to ${rates.toFixed(3)}`;
-    })
+ 
+    document.querySelector('form').onsubmit= () => {
+
+        const c1 =  document.querySelector('#c1').value.toUpperCase();
+        const c2 =  document.querySelector('#c2').value.toUpperCase();
+        const cv1 =  document.querySelector('#cv1').value;
+
+        
+        fetch(`https://api.exchangeratesapi.io/latest?base=${c1}`)
+        .then(response => response.json())
+        
+        .then(data => {
+
+            const rates = data.rates[c2] * cv1;
+            document.querySelector('#output').innerHTML = `${cv1} ${c1} equals to ${rates} ${c2}`;
+
+        })
+
+        return false;
+    };
+
 });
+
+
